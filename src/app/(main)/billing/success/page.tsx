6f1afcee8482152a060a1cb7d35a3d20c7614d48
mrullldhm <mrullldhm94@gmail.com>
2025-07-11 02:26:31 +0800

    Fix billing/success/page.ts

--- src/app/(main)/billing/success/page.tsx
+++ src/app/(main)/billing/success/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function PAge() {
+export default function Page() {
   return (
     <main className="max-w-7xl mx-auto space-y-6 px-3 py-6 text-center">
       <h1 className="text-3xl font-bold">Billing Success</h1>
